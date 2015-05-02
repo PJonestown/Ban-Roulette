d111bc0721a5df5e8e06f3726989391ec12099a0
Newton <anthpaul0@gmail.com>
2015-05-02 15:47:03 -0500

    Sessions controller, change route name for new

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/new', as: 'new_user'
-  get 'users', to: 'users#index'
-  post 'users', to: 'users#create'
+  get 'sign_in',    to:  'sessions#new'
+
+  get 'users/new',  as: 'new_user'
+  get 'users',      to: 'users#index'
+  post 'users',     to: 'users#create'
 end
