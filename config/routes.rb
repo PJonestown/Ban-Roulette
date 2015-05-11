2ebb0e64384d8f41ef341514b0f3e7ff6baaff9f
Newton <anthpaul0@gmail.com>
2015-05-11 17:16:07 -0500

    Navbar

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   get 'users/new',  as: 'new_user'
   get 'users',      to: 'users#index'
   post 'users',     to: 'users#create'
+
+  root              to: 'users#new'
 end
--- spec/features/user_sessions_spec.rb
+++ spec/features/user_sessions_spec.rb
@@ -5,6 +5,7 @@ feature 'user sign in/out proccess' do
   context 'registered guest' do
     it 'should sign in the user' do
       user = create(:user)
+      visit root_path
       click_button 'Sign in'
       expect(current_path).to eq(root_path)
       expect(page).to have_content(user.name)
