e20c48c8a758bb4ca059817e1ded0e3c9b60e7aa
Newton <anthpaul0@gmail.com>
2015-05-13 15:45:19 -0500

    Sign in integration test passes

--- spec/features/user_sessions_spec.rb
+++ spec/features/user_sessions_spec.rb
@@ -1,14 +1,15 @@
 require 'rails_helper'
 
-feature 'user sign in/out proccess' do
+feature 'user sign in/out process' do
 
   context 'registered guest' do
     it 'should sign in the user' do
+      page.driver.options[:headers] = {'REMOTE_ADDR' =>  '192.168.0.1'}
       user = create(:user)
       visit root_path
       click_button 'Sign in'
       expect(current_path).to eq(root_path)
-      expect(page).to have_content(user.name)
+      expect(page).to have_content(user.username)
     end
   end
 end
