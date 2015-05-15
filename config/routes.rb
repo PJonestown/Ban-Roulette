7fdf2eb7449511a7c42f505923cf87a146764ce2
Newton <anthpaul0@gmail.com>
2015-05-15 14:47:06 -0500

    Users show finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
+  root              to: 'users#new'
+  resources :users
+
   post 'sign_in',   to:  'sessions#create'
   delete 'sign_out', to:  'sessions#destroy', as: 'destroy_user_session'
-
-  get 'users/new',  as: 'new_user'
-  get 'users',      to: 'users#index'
-  post 'users',     to: 'users#create'
-
-  root              to: 'users#new'
 end
